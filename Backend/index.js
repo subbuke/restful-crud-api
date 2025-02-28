5e6c1b849b656e07cedb57b47cc94bd72a752188
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-28 12:15:23 +0000

    connected to database

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+require('dotenv').config();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+ 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("database connected successfully")
+}).catch(err => console.log(err))
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("server running at port" + PORT)
+})
